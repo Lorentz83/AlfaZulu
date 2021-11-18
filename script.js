458b2a203bae335efe75042ed430b683205b87c8
Lorenzo Bossi <lorenzobossi83@gmail.com>
2021-11-18 19:49:31 +0000

    Simple highlight in the spelled table.
    Also basic keyboard and mouse integration to move the highlighted row.
    
    Some refactoring to better split dom manipulation from logic.

--- script.js
+++ script.js
@@ -53,34 +53,104 @@ if ('serviceWorker' in navigator) {
 }
 
 
+class SpellingBox {
+    constructor(container) {
+        this.container = container;
+        this.table = null;
+    }
+
+    clear() {
+        this.container.innerHTML = '';
+        this.table = null;
+    }
+
+    addLine(letter, codeWord) {
+        if ( !this.table ) {
+            this.table = document.createElement('table');
+            this.container.appendChild(this.table);
+        }
+        const t = this.table;
+
+        const r = document.createElement('tr');
+        t.appendChild(r);
+        
+        const lBox = document.createElement('td');
+        r.appendChild(lBox);
+        const codeBox = document.createElement('td');
+        r.appendChild(codeBox);
+        
+        lBox.innerText = letter;
+        codeBox.innerText = codeWord;
+    }
+
+    highlightFirst() {
+        const rows = this.container.querySelectorAll('tr')
+        if ( rows.length == 0 ) {
+            return false;
+        }
+        rows.forEach(r => r.classList.remove('highlighted'));
+        rows[0].classList.add('highlighted');
+        return true;
+    }
+
+    moveHighlight(moveNext) {
+        let curr = this.container.querySelector('tr.highlighted');
+        if ( ! curr ) { // if there is no highligh, highligh the first.
+            return this.highlighFirst();
+        }
+        const next = moveNext ? curr.nextElementSibling : curr.previousElementSibling;
+        if ( ! next || next.tagName !== 'TR' ) {
+            return false;
+        }
+        curr.classList.remove('highlighted');
+        next.classList.add('highlighted');
+        return true;
+    }
+}
+
 function init() {
     const userInput = document.querySelector('#to-spell');
+
     const outputBox = document.querySelector('#spelled');
+    const output = new SpellingBox(outputBox);
 
     const spellingOutput = {};
 
     const writeSpelling = function(){
-        outputBox.innerHTML = '';
-        const t = document.createElement('table');
-        outputBox.appendChild(t);
+        output.clear();
         
-        updateSpelling(userInput.value, function(l, code){
-            const r = document.createElement('tr');
-            t.appendChild(r);
-
-            const lBox = document.createElement('td');
-            r.appendChild(lBox);
-            const codeBox = document.createElement('td');
-            r.appendChild(codeBox);
-
-            lBox.innerText += l;
-            codeBox.innerText = code;
-        });
+        for ( const line of updateSpelling(userInput.value) ) {
+            output.addLine(line.letter, line.codeWord);
+        }
     }
 
     writeSpelling();
     
     userInput.addEventListener('input', writeSpelling);
+    userInput.addEventListener('keypress', e => {
+        if (e.key == "Enter") {
+            userInput.blur(); // To make virtual keyboard disappear on mobile.
+            output.highlightFirst();
+        }
+    });
+    userInput.addEventListener('keydown', e => {
+        switch (e.key) {
+        case 'ArrowDown':
+            output.moveHighlight(true);
+            e.preventDefault();
+            return;
+        case 'ArrowUp':
+            output.moveHighlight(false);
+            e.preventDefault();
+            return;
+        }
+    });
+    outputBox.addEventListener('wheel', e => {
+        const nextLetter = e.wheelDelta < 0;
+        if ( output.moveHighlight(nextLetter) ) {
+            e.preventDefault();
+        }
+    });
     document.querySelector('#reset-to-spell').addEventListener('click', function(){
         userInput.value = '';
         writeSpelling(); // Input type="reset" doesn't trigger the event "input" on click;
@@ -89,14 +159,16 @@ function init() {
     makeAccordion();
 }
 
-function updateSpelling(text, addPairFn) {
+function updateSpelling(text) {
+    let ret = [];
     for (var i = 0; i < text.length; i++) {
         const l = text.charAt(i);
         // Remove diacritics https://stackoverflow.com/questions/990904
         const n = l.toUpperCase().normalize('NFD').replace(/\p{Diacritic}/gu, '');
         const r = letterMap.has(n) ? letterMap.get(n) : '[?]';
-        addPairFn(l, r);
+        ret.push({letter: l, codeWord: r});
     }
+    return ret;
 }
 
 
